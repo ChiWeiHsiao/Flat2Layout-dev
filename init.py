5291b31e20ab143308d6b5b14ee87631353df683
sunset <chengsun@gapp.nthu.edu.tw>
2019-08-19 07:10:35 +0800

    Add args.py and init.py

---
+++ init.py
@@ -0,0 +1,67 @@
+import os
+import numpy as np
+import torch
+from torch import optim
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+import model
+from dataset import FlatLayoutDataset
+
+
+def init(args):
+    device = torch.device('cpu' if args.no_cuda else 'cuda')
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    os.makedirs(os.path.join(args.ckpt, args.id), exist_ok=True)
+
+    # Create dataloader
+    dataset_train = FlatLayoutDataset(args.imgroot, args.gtpath,
+                                  hw=(args.main_h, args.main_w),
+                                  flip=args.flip, gamma=args.gamma)
+    loader_train = DataLoader(dataset_train, args.batch_size_train,
+                              shuffle=True, drop_last=True,
+                              num_workers=args.num_workers,
+                              pin_memory=not args.no_cuda,
+                              worker_init_fn=lambda x: np.random.seed())
+    if args.valid_imgroot:
+        dataset_valid = FlatLayoutDataset(args.valid_imgroot, args.valid_gtpath,
+                                      hw=(args.main_h, args.main_w),
+                                      flip=False, gamma=False)
+        loader_valid = DataLoader(dataset_valid, args.batch_size_valid,
+                                  shuffle=False, drop_last=False,
+                                  num_workers=args.num_workers,
+                                  pin_memory=not args.no_cuda)
+    else:
+        loader_valid = None
+
+    # Create model
+    Model = getattr(model, args.net)
+    model_kwargs = {'init_bias': args.init_bias}
+    if args.backbone:
+        model_kwargs['backbone'] = args.backbone
+    if args.dilate_scale:
+        model_kwargs['dilate_scale'] = args.dilate_scale
+    net = Model(**model_kwargs).to(device)
+    assert -1 <= args.freeze_earlier_blocks and args.freeze_earlier_blocks <= 4
+    if args.freeze_earlier_blocks != -1:
+        b0, b1, b2, b3, b4 = net.list_blocks()
+        blocks = [b0, b1, b2, b3, b4]
+        for i in range(args.freeze_earlier_blocks + 1):
+            print('Freeze block%d' % i)
+            for m in blocks[i]:
+                for param in m.parameters():
+                    param.requires_grad = False
+
+    # Create optimizer
+    optimizer = optim.Adam(
+        filter(lambda p: p.requires_grad, net.parameters()),
+        lr=args.lr
+    )
+
+    # Create tensorboard for monitoring training
+    tb_path = os.path.join(args.logs, args.id)
+    os.makedirs(tb_path, exist_ok=True)
+    tb_writer = SummaryWriter(log_dir=tb_path)
+
+    return device, loader_train, loader_valid, net, model_kwargs, optimizer, tb_writer
