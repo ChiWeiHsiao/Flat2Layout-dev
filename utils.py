508ef4ced4eb65301746fd950f7cf81065429cfe
ChiWeiHsiao <kiwi010379@gmail.com>
2019-08-24 04:56:23 +0800

    find samples w/ error annotation

--- utils.py
+++ utils.py
@@ -9,6 +9,27 @@ import model
 import model_HorizonNet
 
 
+def find_invalid_data(gt_path):
+    gt = np.load(gt_path)
+    f = gt['floor_corner']
+    c = gt['ceiling_corner']
+    name = gt['img_name']
+    N = f.shape[0]
+    lst_dup = []
+    lst_err_y = []
+    for i in range(N):
+        cxs = c[i][:, 0]
+        cys = c[i][:, 1]
+        fxs = f[i][:, 0]
+        fys = f[i][:, 1]
+        if len(cxs) != len(np.unique(cxs)) or len(fxs) != len(np.unique(fxs)):
+            lst_dup.append(name[i])
+        if np.any(cys >= 640-1) or np.any(fys <= 0):
+            lst_err_y.append(name[i])
+    return lst_dup, lst_err_y
+
+
+
 def save_model(net, path, kwargs, model_kwargs):
     torch.save(OrderedDict({
         'state_dict': net.state_dict(),
@@ -32,6 +53,43 @@ def load_trained_model(path):
         print(sys.exc_info())
         net.load_state_dict(state_dict['state_dict'], strict=False)
     return net, kwargs
+
+
+def params_to_1d(params, W=1024):
+    '''
+    params: B x 4 x W'
+        - params[:, 0] is v1 in [-pi/2, pi/2]
+        - params[:, 1] is v2 in [-pi/2, pi/2]
+        - params[:, 2] is v4 in [-pi/2, pi/2]
+        - params[:, 3] is u4 in [-1, 1]
+    return: B x 1 x W
+    '''
+    B, _, W_ = params.shape
+    assert _ == 4 and W % W_ == 0
+
+    # Assign zero shifted (u3 = 0) u1v1u2v2 for each output element
+    v1 = torch.zeros([B, 1, W])
+    v2 = torch.zeros([B, 1, W])
+    u1 = torch.zeros([B, 1, W])
+    u2 = torch.zeros([B, 1, W])
+
+    # Generate 1d
+    ones = torch.zeros([B, 1, W, 3])
+    ones[..., 1] = 1
+    xyz1 = torch.stack([
+        torch.cos(v1) * torch.cos(u1),
+        torch.cos(v1) * torch.sin(u1),
+        torch.sin(v1),
+    ])  # B x 1 x W x 3
+    xyz2 = torch.stack([
+        torch.cos(v2) * torch.cos(u2),
+        torch.cos(v2) * torch.sin(u2),
+        torch.sin(v2),
+    ])  # B x 1 x W x 3
+    cross = torch.cross(xyz1, xyz2, dim=3)
+    cross = torch.cross(cross, ones, dim=3)
+    assert cross[..., 1].abs().max().item() < 1e-6
+
 
 
 if __name__ == '__main__':
