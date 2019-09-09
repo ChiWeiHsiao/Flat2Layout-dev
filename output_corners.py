edb1146530f78b290d23ae8ab02b2c8f90a0aab9
Cheng Sun <s2821d3721@gmail.com>
2019-09-09 21:53:48 +0800

    if peak score too small & left/right slopes too close, remove peak

--- output_corners.py
+++ output_corners.py
@@ -19,7 +19,8 @@ from dataset import normalize_rgb
 import eval_utils
 
 
-def find_peaks(signal, winsz=3, min_v=0.05):
+
+def find_peaks(signal, min_v=0.05, winsz=5):
     max_v = maximum_filter(signal, size=winsz)
     pk_loc = np.where(max_v == signal)[0]
     pk_loc = pk_loc[signal[pk_loc] > min_v]
@@ -28,15 +29,15 @@ def find_peaks(signal, winsz=3, min_v=0.05):
 
 def fit_line(xs, ys, mask):
     if len(xs) == 0:
-        return None
+        return None, None
     if mask.sum() < 2:
-        return None
+        return None, None
     xs = xs[mask]
     ys = ys[mask]
     m, b = siegelslopes(ys, xs)
     # line in homogeneous coordinate
     L = np.cross([0, 0*m+b, 1], [1, 1*m+b, 1])
-    return L
+    return L, m
 
 
 def interp_L(L, x=None, y=None):
@@ -54,7 +55,7 @@ def line_X_rectangle(L):
 
 
 def one_line_segment_case(xs, reg1d, mask):
-    L = fit_line(xs, reg1d, mask)
+    L, slope = fit_line(xs, reg1d, mask)
     if L is None:
         return np.array([]).reshape(-1, 2)
     p0, p1 = line_X_rectangle(L)
@@ -63,7 +64,7 @@ def one_line_segment_case(xs, reg1d, mask):
     return np.array([p0, p1]).reshape(-1, 2)
 
 
-def extract_corners(cor1d, reg1d, min_v=0.05):
+def extract_corners(cor1d, reg1d, min_v=0.05, winsz=5, score_thresh=0.5):
     assert len(cor1d.shape) == 1
     assert len(reg1d.shape) == 1
     assert len(reg1d) % len(cor1d) == 0
@@ -71,24 +72,55 @@ def extract_corners(cor1d, reg1d, min_v=0.05):
     N = len(reg1d)
     M = len(cor1d)
     step = N // M
-    pks = [step//2 + v*step for v in find_peaks(cor1d, min_v=min_v)]
+    pks = find_peaks(cor1d, min_v=min_v, winsz=winsz)
+    pks_score = list(cor1d[pks])
+    pks = [step//2 + v*step for v in pks]
     xs = np.linspace(0, 1, N)
 
-    # Special case: no corners found
-    if len(pks) == 0:
-        return one_line_segment_case(xs, reg1d, mask)
- 
-    # Segment indices
     pks = [0, *pks, N-1]
-    segments = []
-    for i in range(1, len(pks)):
-        segments.append([pks[i-1], pks[i]+1])
+    PASS = False
+    while not PASS:
+        # Special case: no corners found
+        if len(pks) == 2:  # [0, N-1]
+            return one_line_segment_case(xs, reg1d, mask)
+     
+        # Segment indices
+        segments = []
+        for i in range(1, len(pks)):
+            segments.append([pks[i-1], pks[i]+1])
 
-    # Find line for each segment
-    Ls = [
-        fit_line(xs[s:t], reg1d[s:t], mask[s:t])
-        for s, t in segments
-    ]
+        # Find line for each segment
+        Ls = []
+        slopes = []
+        for s, t in segments:
+            L, m = fit_line(xs[s:t], reg1d[s:t], mask[s:t])
+            Ls.append(L)
+            slopes.append(m)
+
+        # Check: if pks_score small & l/r slopes close -> remove peaks, merge segment
+        PASS = True
+        delete_idxs = []
+        for i in range(len(pks_score)):
+            # 0.5 = 26.57° # 0.364 = 20° # 0.268 = 15° # 0.176 = 10° # 0.0875 = 5° # 0.0349 = 2°
+            fail = (slopes[i] and slopes[i+1]) and (
+                    mask[segments[i][0]:segments[i][1]].sum()>2*step and
+                    mask[segments[i+1][0]:segments[i+1][1]].sum()>2*step) and (
+                    (pks_score[i] < 0.5 and np.abs(slopes[i]-slopes[i+1]) < 0.176)
+                    or (pks_score[i] < 0.2 and np.abs(slopes[i]-slopes[i+1]) < 0.268)
+                    or (pks_score[i] < 0.1 and np.abs(slopes[i]-slopes[i+1]) < 0.364)
+                    or (pks_score[i] < 0.075 and np.abs(slopes[i]-slopes[i+1]) < 0.5)
+                    )
+            if fail: #pks_score[i] < score_thresh and slopes[i] and slopes[i+1] and np.abs(slopes[i]-slopes[i+1]) < 0.5:
+                print('Pks_score small & l/r Slopes close: ', i, pks_score[i], slopes[i], slopes[i+1])
+                PASS = False
+                delete_idxs.append(i)
+        for idx in reversed(delete_idxs):
+            pks_score.pop(idx)
+            print('pks=', pks, end='\t')
+            pks.pop(idx+1)
+            print('-> pks=', pks)
+    assert(len(pks_score) == len(pks)-2)
+    assert(pks[0]==0 and pks[-1]==N-1)
 
     # Connect line into corners
     if len(Ls) == 2 and (Ls[0] is None or Ls[1] is None):
@@ -103,8 +135,11 @@ def extract_corners(cor1d, reg1d, min_v=0.05):
         if len(cor_lr) > 0:
             return np.vstack([cor_lr[[0]], cor_mid, cor_lr[[1]]])
         else:
-            # TODO check corner peak confidence: if small->only 1, if large->repeat 3
-            return np.vstack([cor_mid, cor_mid, cor_mid])
+            # check corner peak confidence: if small->only 1, if large->repeat 3
+            if pks_score[0] < score_thresh:
+                return cor_mid
+            else:
+                return np.vstack([cor_mid, cor_mid, cor_mid])
 
     for i in range(1, len(Ls)-1):
         assert Ls[i] is not None, 'Middle segments dont have line !??'
@@ -112,7 +147,8 @@ def extract_corners(cor1d, reg1d, min_v=0.05):
     if Ls[0] is None:
         p0, p1 = line_X_rectangle(Ls[1])
         corners.append(p0)
-        corners.append(p0)
+        if pks_score[0] > score_thresh:
+            corners.append(p0)
         #  raise ValueError('Ls[0] == None')
     else:
         p0, p1 = line_X_rectangle(Ls[0])
@@ -126,19 +162,19 @@ def extract_corners(cor1d, reg1d, min_v=0.05):
 
         # If intersection x is too far from predicted y_cor, xy=[y_cor, y_reg[y_cor]]
         toofar = 16
+        pts = pts[:2] / pts[2]  # [x,y,z]->[x/z,y/z]
         if np.abs(N*pts[0] - pks[i]) < toofar:
-            corners.append(pts[:2] / pts[2])
+            corners.append(pts[:2])
         else:
             pred_x = pks[i]
-            #  print('intersection too far')
-            #  print('intersect:', pts[:2] / pts[2] * N)
-            #  print('new      :', [pred_x, reg1d[pred_x]*N])
-            corners.append(np.array([pred_x/N, reg1d[pred_x]]))
+            pred_y = min(max(reg1d[pred_x], 0), 1)
+            corners.append(np.array([pred_x/N, pred_y]))
     
     if Ls[-1] is None:
         p0, p1 = line_X_rectangle(Ls[-2])
         corners.append(p1)
-        corners.append(p1)
+        if pks_score[-1] > score_thresh:
+            corners.append(p1)
     else:
         p0, p1 = line_X_rectangle(Ls[-1])
         corners.append(p1)
@@ -146,28 +182,13 @@ def extract_corners(cor1d, reg1d, min_v=0.05):
     # Check failure
     for c in corners:
         if c[0] is None or c[0] < -0.1 or c[0] > 1.1 or c[1] < -0.1 or c[1] > 1.1:
+            print(c)
+            print('corners: ', corners)
+            print('Failure: c[0] is None or c[0] < -0.1 or c[0] > 1.1 or c[1] < -0.1 or c[1] > 1.1')
             return one_line_segment_case(xs, reg1d, mask)
 
-    #  # Check corners if too close, delete one
-    #  corners = np.array(corners).reshape(-1, 2)
-    #  nC = corners.shape[0]
-    #  dist = cdist(corners, corners)
-    #  dist = dist + 1e9*np.eye(nC)
-    #  amin = dist.argmin(0)
-    #  tooclose = dist[np.arange(nC), amin] < 0.016
-    #  if tooclose.sum():
-        #  print('too close, dist=: ', dist[tooclose, amin[tooclose]])
-        #  print('\nori corners:', corners*640)
-        #  for i in range(nC):
-            #  if tooclose[i]:
-                #  print('Pair ', i, 640*corners[i], 640*corners[amin[i]])
-                #  tmp_x, tmp_y = corners[i]
-                #  if tmp_x==0 or tmp_x==1 or tmp_y==0 or tmp_y==1:
-                    #  tooclose[i] = False
-        #  corners = corners[~tooclose]
-        #  print('\nnew corners:', corners*640)
-    return np.array(corners).reshape(-1, 2)
 
+    return np.array(corners).reshape(-1, 2)
 
 
 if __name__ == '__main__':
@@ -182,6 +203,7 @@ if __name__ == '__main__':
     parser.add_argument('--gtpath', default='datas/lsun/validation.npz')
     parser.add_argument('--no_cuda', action='store_true')
     parser.add_argument('--min_v', default=0.05, type=float)
+    parser.add_argument('--winsz', default=5, type=int)
     parser.add_argument('--debug')
     parser.add_argument('--vis', action='store_true')
     args = parser.parse_args()
@@ -233,9 +255,12 @@ if __name__ == '__main__':
 
         # Prepare output
         with torch.no_grad():
-            out_reg, out_cor = net(x[None])
-            print('out_reg: ', out_reg.shape)
-            print('out_cor: ', out_cor.shape)
+            output = net(x[None])
+            if len(output) == 3:
+                out_reg, out_cor, out_key = output
+            else:
+                out_reg, out_cor = output
+                out_key = []
             # Upsample W/32 --> W
             y_step = w // out_reg.shape[-1]
             pad_left = (2*out_reg[...,[0]]-out_reg[...,[1]])
@@ -246,9 +271,11 @@ if __name__ == '__main__':
             #  out_reg = F.interpolate(out_reg, size=w, mode='linear', align_corners=False)
             np_reg = out_reg[0].cpu().numpy() / 2 + 0.5  # [-1, 1] => [0, 1]
             np_cor = torch.sigmoid(out_cor[0]).cpu().numpy()
+            if len(out_key) > 0:
+                np_key = torch.sigmoid(out_key[0]).cpu().numpy()
 
-        corners_c = extract_corners(np_cor[0], np_reg[0], min_v=args.min_v)
-        corners_f = extract_corners(np_cor[1], np_reg[1], min_v=args.min_v)
+        corners_c = extract_corners(np_cor[0], np_reg[0], min_v=args.min_v, winsz=args.winsz)
+        corners_f = extract_corners(np_cor[1], np_reg[1], min_v=args.min_v, winsz=args.winsz)
         if len(corners_c) != 0 and len(corners_f) == 0:
             corners_f = np.array([
                 [x, 1]
@@ -287,6 +314,16 @@ if __name__ == '__main__':
             u_cor[..., [0,2]] = 0  # keep only G
             d_cor[..., [0,1]] = 0  # keep only B
             rgb = np.vstack([rgb, u_cor, d_cor])
+            if len(out_key) > 0:
+                u_key, d_key = np_key.repeat(32, axis=-1)
+                u_key = u_key.reshape([1,-1]).repeat(10, axis=0)
+                d_key = d_key.reshape([1,-1]).repeat(10, axis=0)
+                u_key = u_key.reshape([10, w, 1]).repeat(3, axis=2)
+                d_key = d_key.reshape([10, w, 1]).repeat(3, axis=2)
+                u_key[..., [2]] = 0  # R+G
+                d_key[..., [1]] = 0  # R+B
+                rgb = np.vstack([rgb, u_key, d_key])
+
             plt.imshow(np.clip(rgb, 0, 1))
             plt.plot(np.linspace(0, 1, w) * w, np_reg[0] * h, 'g')
             plt.plot(np.linspace(0, 1, w) * w, np_reg[1] * h, 'b')
